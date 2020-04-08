44da98404f637d5e0ef6488d82f5bd495dc4735a
Fabio Di Gennaro <fabiodigennaro14@gmail.com>
2020-04-08 07:58:50 +0200

    Aggiunto messaggio javascript

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
 // Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e cliccando invia il testo viene aggiunto al thread sopra, come messaggio verde.
 
-//
+
+
+
+
+// rendere il form scrivibile e alla fine del messaggio dare all'icona invio l'evento click. Poi salvare il value di quanto inserito in un p. Oppure creare nella funzione.
+
+
+
+$( document ).ready(function() {
+
+    // do l'evento click all icona di invio
+    $('.fa-telegram-plane').click(
+      function(){
+        // salvo il valore dell'input in una variabile
+          var messInv = $("#testojs").val();
+          console.log(messInv);
+          $('#outputjs').append(messInv).addClass('green-message');
+      }
+  )
+
+
+
+
+
+
+});
