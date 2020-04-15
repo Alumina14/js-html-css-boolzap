9ac8a43b8fe87ffbe6626a18cfceeb09b65a4aba
Fabio Di Gennaro <fabiodigennaro14@gmail.com>
2020-04-15 16:46:16 +0200

    upload esercizio

--- js/main.js
+++ js/main.js
@@ -1,41 +1,96 @@
 // Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e cliccando invia il testo viene aggiunto al thread sopra, come messaggio verde.
 
 
-
-
-
-// rendere il form scrivibile e alla fine del messaggio dare all'icona invio l'evento click. Poi salvare il value di quanto inserito in un p. Oppure creare nella funzione.
-
-
-
 $( document ).ready(function() {
 
+//  salvo riferimenti per poter salvare mess precedenti e dove stampare il mess
+
+var msg = $('#testojs');
+var outputJs = $('#outputjs'); // output mess verde
+var outputAuto = $('#outputauto'); // output mess bianco
+var contatti = $('.contatti'); // var per filtro contatti
 
     // do l'evento click all icona di invio
     $('.fa-telegram-plane').click(
       function(){
         // salvo il valore dell'input in una variabile
-          var messInv = $("#testojs").val();
+          var messInv = msg.val();
           console.log(messInv);
-          $('#outputjs').append(messInv).addClass('green-message');
+          outputJs.append('<div class="green-message">' + messInv + '<i class="fa fa-chevron-down f-right"></i>' + '</div>' + '<div class="opzioni">' +  "Cancella" +
+          '</div>');
+          msg.val(""); // cancello il msg precedentemente scritto dall'input form
+
+          // messaggio automatico ok
+        setTimeout(
+          function(){
+
+        outputAuto.append('<div class="white-message2">' + "Ok" + '<i class="fa fa-chevron-down f-right"></i>' + '</div>' + '</div>' + '<div class="opzioni">' +  "Cancella" +
+        '</div>');
+          }
+        ,1000)
+
       }
+
+
   )
 
-    // $('#testojs').focusin(
+// funzione ricerca contatti
+
+$('#filtro').keyup(
+  function(){
+// salvo input valore della ricerca
+var ricercaCont = $(this).val().toLowerCase();
+
+  contatti.each(
+  function () {
+
+var nomeCont = $(this).find('.nome-contatto').text().toLowerCase();
+
+    if(nomeCont.includes(ricercaCont)){
+
+      $(this).show();
+    } else {
+      $(this).hide();
+    }
+
+});
+
+});
+
+// mostrare contatto cliccato
+$('.contatti').click(
+function () {
+  contatti.removeClass("active");
+  $(this).addClass("active");
+  var attr = $(this).data("conversazione");
+
+  outputJs.removeClass("active");
+  $('#outputjs[data-conversazione="' + attr + '"]').addClass("active");
+
+});
+
+
+
+
+$('#outputjs').mouseover(
+  function(){
+$('.fa.fa-chevron-down.f-right').toggle();
+
+  });
+
+  $('#outputjs').mouseleave(
+    function(){
+  $('.fa.fa-chevron-down.f-right').hide();
+
+    });
+
+
+    // $('.fa.fa-chevron-down.f-right').click(
     //   function(){
-    //       $('.fa-telegram-plane').show();
-    //       $('.fa-microphone').hide();
-    //   }
+    // $('.opzioni').show();
     //
-    // )
-    //
-    // $('#testojs').focusout(
-    //   function(){
-    //       $('.fa-telegram-plane').hide();
-    //       $('.fa-microphone').show();
-    //   }
-    //
-    // )
+    //   });
+
 
 
 });
